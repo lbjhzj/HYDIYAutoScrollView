0e1a09a25e4cd5a7efffdc30864ac3ad2831a009
lbjhzj <bryanthao@sina.com>
2016-05-11 19:02:47 +0800

    has

--- HYDIYAutoScrollView/ViewController.m
+++ HYDIYAutoScrollView/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    This is a TEST
+
 }
 
 - (void)didReceiveMemoryWarning {
